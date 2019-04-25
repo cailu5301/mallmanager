980f48dcfc9d1d6963954006475757061aafb870
cailu <cailu5301@gmail.com>
2019-04-25 20:34:51 +0800

    登录路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login'
 
 Vue.use(Router)
 
@@ -7,7 +8,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld'
+      name: 'Home'
+    }, {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
