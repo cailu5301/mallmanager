d778c1066de6c21e6761ac902610e737c57ebbb7
cailu <cailu5301@gmail.com>
2019-04-25 21:40:28 +0800

    登录界面样式基本完成

---
+++ src/components/Login/login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="login-wrapper">
+    <el-form :model="loginForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="loginForm" size="medium">
+      <h2>用户登录界面</h2>
+      <el-form-item label="用户名" prop="username">
+        <el-input type="password" v-model="loginForm.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="loginForm.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" class="btn">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      ruleForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          {required: true, message: '请输入用户名', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'}
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login-wrapper {
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.loginForm {
+  background-color: white;
+  width: 500px;
+  height: 300px;
+  border-radius: 5px;
+  padding: 20px;
+}
+.btn {
+  width: 100%;
+}
+</style>
