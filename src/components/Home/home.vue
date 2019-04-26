a7fc49e46fc21a5492b472a80fc5875ce38cf68b
cailu <cailu5301@gmail.com>
2019-04-26 23:24:08 +0800

    主页头部样式调整

--- src/components/Home/home.vue
+++ src/components/Home/home.vue
@@ -1,6 +1,18 @@
 <template>
   <el-container class="container">
-    <el-header>Header</el-header>
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+          <img src="../../assets/logo.png" width="50%"  class="image">
+        </el-col>
+        <el-col :span="18">
+          <h2 class="title">电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="2">
+          <a href="#" class="logout">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside class="aside" width="200px">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -24,5 +36,16 @@ export default {
 }
 .main {
   background-color: powderblue;
+}
+.image {
+  margin-top: 10px;
+}
+.title {
+  text-align: center;
+}
+.logout {
+  line-height: 60px;
+  text-decoration: none;
+  color: black;
 }
 </style>
